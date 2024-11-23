50dcfaf6606b28933298034fea0264872c6901c2
Your Name <you@example.com>
2024-11-22 18:03:44 -0800

    [CLEANUP]

---
+++ examples.py
@@ -0,0 +1,131 @@
+import os
+from dotenv import load_dotenv
+from swarms import Agent, SequentialWorkflow
+from swarm_models import OpenAIChat
+from swarm_deploy import SwarmDeploy
+
+load_dotenv()
+
+# Get the OpenAI API key from the environment variable
+api_key = os.getenv("GROQ_API_KEY")
+
+# Model
+model = OpenAIChat(
+    openai_api_base="https://api.groq.com/openai/v1",
+    openai_api_key=api_key,
+    model_name="llama-3.1-70b-versatile",
+    temperature=0.1,
+)
+
+
+# Initialize specialized agents
+data_extractor_agent = Agent(
+    agent_name="Data-Extractor",
+    system_prompt=None,
+    llm=model,
+    max_loops=1,
+    autosave=True,
+    verbose=True,
+    dynamic_temperature_enabled=True,
+    saved_state_path="data_extractor_agent.json",
+    user_name="pe_firm",
+    retry_attempts=1,
+    context_length=200000,
+    output_type="string",
+)
+
+summarizer_agent = Agent(
+    agent_name="Document-Summarizer",
+    system_prompt=None,
+    llm=model,
+    max_loops=1,
+    autosave=True,
+    verbose=True,
+    dynamic_temperature_enabled=True,
+    saved_state_path="summarizer_agent.json",
+    user_name="pe_firm",
+    retry_attempts=1,
+    context_length=200000,
+    output_type="string",
+)
+
+financial_analyst_agent = Agent(
+    agent_name="Financial-Analyst",
+    system_prompt=None,
+    llm=model,
+    max_loops=1,
+    autosave=True,
+    verbose=True,
+    dynamic_temperature_enabled=True,
+    saved_state_path="financial_analyst_agent.json",
+    user_name="pe_firm",
+    retry_attempts=1,
+    context_length=200000,
+    output_type="string",
+)
+
+market_analyst_agent = Agent(
+    agent_name="Market-Analyst",
+    system_prompt=None,
+    llm=model,
+    max_loops=1,
+    autosave=True,
+    verbose=True,
+    dynamic_temperature_enabled=True,
+    saved_state_path="market_analyst_agent.json",
+    user_name="pe_firm",
+    retry_attempts=1,
+    context_length=200000,
+    output_type="string",
+)
+
+operational_analyst_agent = Agent(
+    agent_name="Operational-Analyst",
+    system_prompt=None,
+    llm=model,
+    max_loops=1,
+    autosave=True,
+    verbose=True,
+    dynamic_temperature_enabled=True,
+    saved_state_path="operational_analyst_agent.json",
+    user_name="pe_firm",
+    retry_attempts=1,
+    context_length=200000,
+    output_type="string",
+)
+
+# Initialize the SwarmRouter
+router = SequentialWorkflow(
+    name="pe-document-analysis-swarm",
+    description="Analyze documents for private equity due diligence and investment decision-making",
+    max_loops=1,
+    agents=[
+        data_extractor_agent,
+        summarizer_agent,
+        financial_analyst_agent,
+        market_analyst_agent,
+        operational_analyst_agent,
+    ],
+    output_type="all",
+)
+
+# Advanced usage with configuration
+swarm = SwarmDeploy(
+    router,
+    max_workers=4,
+    # cache_backend="redis"
+)
+swarm.start(
+    host="0.0.0.0",
+    port=8000,
+    workers=4,
+    # ssl_keyfile="key.pem",
+    # ssl_certfile="cert.pem"
+)
+
+# # Create a cluster
+# instances = SwarmDeploy.create_cluster(
+#     your_callable,
+#     num_instances=3,
+#     start_port=8000
+# )
